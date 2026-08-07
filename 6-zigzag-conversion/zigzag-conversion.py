3bf8c119890e78b8098e91ef08fa93de2dd9c607
prakashkundu <prakash.kundu2005@gmail.com>
2026-08-08 00:20:04 +0530

    Time: 8 ms (84.31%) | Memory: 12.5 MB (55.98%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,6 +1,8 @@
 class Solution(object):
     def convert(self, s, numRows):
-        
+        if numRows == 1 or numRows >= len(s):
+            return s
+
         t = list(range(numRows)) + list(range(numRows - 2, 0, -1))
 
         r = [""] * numRows
