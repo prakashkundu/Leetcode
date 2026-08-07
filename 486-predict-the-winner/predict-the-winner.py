8a8ae193dea3b56542ff3439f5975ce15696305e
prakashkundu <prakash.kundu2005@gmail.com>
2026-08-08 03:05:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (93.95%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def predictTheWinner(self, nums):
+        n = len(nums)
+        dp = nums[:]
+
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n):
+                dp[j] = max(
+                    nums[i] - dp[j],
+                    nums[j] - dp[j - 1]
+                )
+
+        return dp[n - 1] >= 0
