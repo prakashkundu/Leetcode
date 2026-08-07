228dd688e6eef5b4495b6c9df055a473661b6c3a
prakashkundu <prakash.kundu2005@gmail.com>
2026-08-08 00:19:42 +0530

    Time: 13 ms (50.29%) | Memory: 12.6 MB (19.37%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        
+        t = list(range(numRows)) + list(range(numRows - 2, 0, -1))
+
+        r = [""] * numRows
+
+        for i, char in enumerate(s):
+            r[t[i % len(t)]] += char
+
+        return ''.join(r)
+        
