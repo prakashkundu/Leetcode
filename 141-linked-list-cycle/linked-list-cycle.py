b846a217b1fa8c51a2d5a1eb7190f438292a947b
prakashkundu <prakash.kundu2005@gmail.com>
2026-08-19 10:42:54 +0530

    Time: 39 ms (43.80%) | Memory: 19.5 MB (65.83%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -12,8 +12,6 @@ class Solution(object):
         while (fast != None and fast.next != None):
             slow = slow.next
             fast = fast.next.next
-
             if slow == fast:
                 return True
-
         return False
