9cf6143b764ede49233e87c1bc0b00e74b2993ae
prakashkundu <prakash.kundu2005@gmail.com>
2026-08-26 11:35:40 +0530

    Time: 3 ms (59.25%) | Memory: 52.6 MB (46.19%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -2,7 +2,6 @@ class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int count = 0;
         int max_count = 0;
-
         for (int num : nums) {
             if (num == 1) {
                 count++;
@@ -11,7 +10,6 @@ class Solution {
                 count = 0;
             }
         }
-
         return max_count;
     }
 }
